5793b8c2559eeae9fb493234d5ab78e9e9f8a654
Daniel Barta <daniel@slateteams.com>
2024-08-26 15:18:38 +0200

    fix: add module provider

--- projects/ngx-drag-resize-demo/src/app/app.config.ts
+++ projects/ngx-drag-resize-demo/src/app/app.config.ts
@@ -1,8 +1,9 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { NgxDragResizeModule } from 'ngx-drag-resize';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), importProvidersFrom(NgxDragResizeModule)]
 };
